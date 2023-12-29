7423b9fb0d2fe8ace76966835a528ce06af58bfd
David Rivera <jherax@gmail.com>
2023-12-28 20:58:09 -0500

    feat: Added Apollo Sandbox in '/playground' path

--- src/appRouter.tsx
+++ src/appRouter.tsx
@@ -6,13 +6,19 @@ import {
 } from 'react-router-dom';
 
 import Autocomplete from './views/Autocomplete';
+import {ErrorBoundary} from './views/ErrorBoundary';
+import {Playground} from './views/Playground';
 import Rentalscape from './views/Rentalscape';
 
+/**
+ * @see https://reactrouter.com/en/main/route/route
+ */
 const AppRouter = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Outlet />}>
+    <Route path='/' element={<Outlet />} errorElement={<ErrorBoundary />}>
       <Route path='' element={<Autocomplete maxWidth='500px' />} />
       <Route path='/mapbox' element={<Rentalscape />} />
+      <Route path='/playground/:gqlConn' element={<Playground />} />
     </Route>,
   ),
 );
